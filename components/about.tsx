ac31edfcee3cb205d023e074aa387ab1b82e0252
Mohiudin <ranamohiudin1234@gmail.com>
2025-03-06 11:41:17 +0500

    Update file

--- components/about.tsx
+++ components/about.tsx
@@ -5,11 +5,7 @@ export default function About() {
                 <div className="space-y-4">
                     <h1 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">About me</h1>
                     <p className="max-w-[600px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-<<<<<<< HEAD
-                    Hey! I'm Mohiudin. I'm very enthusiastic about my web development career and continuously evolving myself according to innovations in the world of web development. The technologies I’m currently working with are <span className="font-bold text-red-500 "> TypeScript, JavaScript, React, and Node.js.</span>
-=======
                     Hey! I&apos;m Mohiudin. I&apos;m very enthusiastic about my web development career and continuously evolving myself according to innovations in the world of web development. The technologies I&apos;m currently working with are <span className="font-bold text-red-500 "> TypeScript, JavaScript, React, and Node.js.</span>
->>>>>>> 203b6a5 (Updated files with new changes)
 
                         <br />
                         <br />
@@ -20,21 +16,13 @@ export default function About() {
                     <div className="grid gap-1">
                         <h2 className="text-xl font-bold">Background</h2>
                         <p className="text-muted-foreground">
-<<<<<<< HEAD
-                            I'm a final-year <span className="font-bold">Computer Science</span> student. I'm kickstarting my career as a web developer because the web is becoming more diverse day by day.
-=======
                             I&apos;m a final&dash;year <span className="font-bold">Computer Science</span> student. I&apos;m kickstarting my career as a web developer because the web is becoming more diverse day by day.
->>>>>>> 203b6a5 (Updated files with new changes)
                         </p>
                     </div>
                     <div className="grid gap-1">
                         <h2 className="text-xl font-bold">Skills</h2>
                         <p className="text-muted-foreground">
-<<<<<<< HEAD
-                            My main skills? i'm really good with Typescript, Javascript, React, and Node.js. I also know my way around Nextjs, Expressjs, and a bunch of other cool libraries. I can make websites look great on any device. Oh, and i'm pretty handy with git for keeping track of all my code changes.
-=======
                             My main skills? i&apos;m really good with Typescript, Javascript, React, and Node.js. I also know my way around Nextjs, Expressjs, and a bunch of other cool libraries. I can make websites look great on any device. Oh, and i&apos;m pretty handy with git for keeping track of all my code changes.
->>>>>>> 203b6a5 (Updated files with new changes)
                         </p>
                     </div>
 
